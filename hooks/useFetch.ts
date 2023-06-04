fdd3eb97c88dafc9656e13af52fc962a3b374a2c
Emerlyy <vadim200339@gmail.com>
2023-06-04 22:24:31 +0300

    created

---
+++ hooks/useFetch.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+type LoadingStatus = 'loading' | 'received' | 'rejected';
+
+type FetchResponse<T> = {
+    data: null | T,
+    status: LoadingStatus,
+    error: null | Error,
+}
+
+export const useFetch = <T>(url: string): FetchResponse<T> => {
+    const [response, setResponse] = useState<FetchResponse<T>>({
+        data: null,
+        status: 'loading',
+        error: null
+    });
+
+    useEffect(() => {
+        let isSubscribed = true;
+
+        const fetchData = async (): Promise<void> => {
+            setResponse({ data: null, status: 'loading', error: null });
+            try {
+                const res = await fetch(url);
+                const data = await res.json() as T;
+
+                if (isSubscribed) {
+                    setResponse({ data, status: 'received', error: null });
+                }
+            } catch (error) {
+                if (isSubscribed && error instanceof Error) {
+                    setResponse({ data: null, status: 'rejected', error });
+                    console.error(error.name + '---' + error.message);
+                }
+            }
+        }
+
+        fetchData();
+
+        return () => {
+            isSubscribed = false;
+        }
+    }, [])
+
+    return response;
+}
