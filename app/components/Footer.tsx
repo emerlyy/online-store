e8ed0349fecfa6bf4b470295434102087589ea13
Emerlyy <vadim200339@gmail.com>
2023-06-04 22:20:40 +0300

    created footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return (
+        <footer className="min-h-[80px] bg-black p-6">
+            <h2 className="text-white text-lg">Made by Emerly</h2>
+        </footer>
+    )
+}
+
+export default Footer;
