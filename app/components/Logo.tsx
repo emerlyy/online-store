e255ff97d47830039e00ce1edc3726fdd961dcb6
Emerlyy <vadim200339@gmail.com>
2023-06-06 16:14:22 +0300

    added secondary color variant

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { FaShopify } from "react-icons/fa";
 
-type LogoColor = 'white' | 'black';
+type LogoColor = 'white' | 'black' | 'secondary';
 
 interface Props {
     color?: LogoColor
 }
 
 const Logo = ({ color = 'black' }: Props) => {
-
+    const logoColor = color === 'secondary' ? '[--secondary-color]' : color;
     return (
-        <Link href='/' className={`text-${color} flex items-center gap-x-3 text-2xl font-bold tracking-widest`}>
-            <FaShopify className="text-5xl"/>
+        <Link href='/' className={`text-${logoColor} flex items-center gap-x-3 text-2xl font-bold tracking-widest`}>
+            <FaShopify className="text-5xl" />
             Emerly shop
         </Link>
     )
